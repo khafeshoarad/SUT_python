73d87186beec695c2d462d4e8d26d8a8d6941adb
khafeshoarad <154825878+khafeshoarad@users.noreply.github.com>
2024-01-26 14:25:15 +0330

    Update gholami.py
    refactored version

--- exersice 1/gholami.py
+++ exersice 1/gholami.py
@@ -1,24 +1,18 @@
-def toBinary(n): 
+def to_binary(n):
     return "{:032b}".format(int(n))
-rast = str(toBinary(int(str(input()))))
-chap = str(toBinary(int(str(input()))))
-dt = str(chap)+str(rast)
-ns = []
-for _ in range(int(input())):
-    ns.append(int(input()))
-for n in ns:
-    # print(str(n) + ":" + shirzad[len(shirzad) - n - 1])
-    if dt[-n-1] == "0":
-        print("no")
-    else:
-        print("yes")
 
-# list=[]
-# for i in range(n):
-#     m = int(input())
-#     if dt[-m-1]=='0':
-#         list.append('no')
-#     else:
-#         list.append('yes')
-# for a in range(len(list)):
-#     print(list[a]) 
+def main():
+    rast = to_binary(input("Enter the first number: "))
+    chap = to_binary(input("Enter the second number: "))
+    dt = chap + rast
+
+    ns = []
+    for _ in range(int(input("Enter the number of queries: "))):
+        ns.append(int(input("Enter a query number: ")))
+
+    for n in ns:
+        result = "yes" if dt[-n-1] == "1" else "no"
+        print(result)
+
+if __name__ == "__main__":
+    main()
