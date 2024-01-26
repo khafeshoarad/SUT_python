7e05f010c1551bfcdf8e31e87260da24e2347342
khafeshoarad <154825878+khafeshoarad@users.noreply.github.com>
2024-01-26 14:34:35 +0330

    Update pat(1).py
    refactored version

--- exersice 3/pat(1).py
+++ exersice 3/pat(1).py
@@ -1,6 +1,9 @@
-vorudi = str(input())
-code = vorudi.split(" ")
-code.sort(key = lambda x: int(x[1:]))
-for i in code:
-    print (i[0],end="")
+input_string = input("Enter space-separated numbers: ")
+numbers = input_string.split(" ")
 
+# Sort the numbers based on their integer values
+sorted_numbers = sorted(numbers, key=lambda x: int(x[1:]))
+
+# Print the sorted numbers without their integer values
+for item in sorted_numbers:
+    print(item[0], end="")
