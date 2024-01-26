4e530614062a9c98b325481d0bc273a26b82d948
khafeshoarad <154825878+khafeshoarad@users.noreply.github.com>
2024-01-26 14:43:48 +0330

    Update q1(1).py
    refactored version

--- exersice 4/q1(1).py
+++ exersice 4/q1(1).py
@@ -2,17 +2,14 @@ import numpy as np
 
 def calculate_determinant(matrix):
     return np.linalg.det(matrix)
-  
-def find_max_determinant_matrices(matrices):
 
+def find_max_determinant_matrices(matrices):
     max_determinant = float('-inf')
     max_matrices = None
 
     for i in range(len(matrices)):
         for j in range(i + 1, len(matrices)):
             product_matrix = np.dot(matrices[i], matrices[j])
-
-
             determinant = calculate_determinant(product_matrix)
 
             if determinant > max_determinant:
@@ -21,35 +18,33 @@ def find_max_determinant_matrices(matrices):
 
     return max_matrices  
 
-n, m = map(int,input().split())
+def input_matrices(n, m):
+    matrices = []
+    for _ in range(n):
+        matrix = [list(map(float, input().split())) for _ in range(m)]
+        matrices.append(matrix)
+    return matrices
 
-matrices = []
-
-for number in range(n):
+def main():
+    n, m = map(int, input("Enter the dimensions (n m): ").split())
+    matrices = input_matrices(n, m)
     
-    matrix = []
-    for i in range(m):
-        
-        row_values = [float(value) for value in input().split()]
-        matrix.append(row_values)
+    max_determinant_matrices = find_max_determinant_matrices(matrices)
     
-    matrices.append(matrix)       
+    det1 = calculate_determinant(max_determinant_matrices[0])
+    det2 = calculate_determinant(max_determinant_matrices[1])
 
-max_determinant_matrices = find_max_determinant_matrices(matrices)
-
-det1 = calculate_determinant(max_determinant_matrices[0])
-det2 = calculate_determinant(max_determinant_matrices[1])
-
-res = np.array([])
-if det1 > det2:
-    res = np.linalg.inv(np.dot(max_determinant_matrices[0], max_determinant_matrices[1]))
-elif det2 > det1:
-    res = np.linalg.inv(np.dot(max_determinant_matrices[1], max_determinant_matrices[0]))
-else:
-    res = np.linalg.inv(np.dot(max_determinant_matrices[1], max_determinant_matrices[0]))
+    if det1 > det2:
+        result_matrix = np.linalg.inv(np.dot(max_determinant_matrices[0], max_determinant_matrices[1]))
+    elif det2 > det1:
+        result_matrix = np.linalg.inv(np.dot(max_determinant_matrices[1], max_determinant_matrices[0]))
+    else:
+        result_matrix = np.linalg.inv(np.dot(max_determinant_matrices[1], max_determinant_matrices[0]))
     
-np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
+    np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
+    
+    formatted_matrix = '\n'.join(' '.join(map(str, row)) for row in result_matrix)
+    print(formatted_matrix)
 
-# Convert the matrix to the desired format
-formatted_matrix = '\n'.join(' '.join(map(str, row)) for row in res)
-print(formatted_matrix)
+if __name__ == "__main__":
+    main()
