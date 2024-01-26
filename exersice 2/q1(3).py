fa3a5b157a08fcd9cb5b10fa56123dbc3448fc56
khafeshoarad <154825878+khafeshoarad@users.noreply.github.com>
2024-01-26 14:32:26 +0330

    Update q1(3).py
    refactored version

--- exersice 2/q1(3).py
+++ exersice 2/q1(3).py
@@ -1,60 +1,61 @@
 def calculate_sum(arr):
-  return sum(arr)
+    return sum(arr)
 
 def calculate_min(arr):
-  return min(arr)
+    return min(arr)
 
 def calculate_max(arr):
-  return max(arr)
+    return max(arr)
 
-def average(arr):
-  
-  avg = sum(arr) / len(arr)
-  return round(avg, 2)
+def calculate_average(arr):
+    avg = sum(arr) / len(arr)
+    return round(avg, 2)
 
 def gcd(x, y):
-  while(y): 
-      x, y = y, x % y 
-  return x 
+    while y:
+        x, y = y, x % y
+    return x
 
 def calculate_gcd(arr):
-   res = int(arr[0])
-    
-   for i in range(1, len(arr)):
-      res = gcd(res, int(arr[i]))
-    
-   return res
+    res = int(arr[0])
+    for i in range(1, len(arr)):
+        res = gcd(res, int(arr[i]))
+    return res
 
 def calculate_lcm(arr):
     lcm = arr[0]
     for i in arr[1:]:
-        lcm = lcm*i//gcd(lcm, i)
+        lcm = lcm * i // gcd(lcm, i)
     return lcm
 
-cmds =  ["min", "max", "lcd", "sum", "average","gcd"]
-cmd = str(input())
+def main():
+    cmds = ["min", "max", "lcd", "sum", "average", "gcd"]
+    cmd = input()
 
-if cmd in cmds:
-    arr = []
-    while(True):
-        user_in = input()
+    if cmd in cmds:
+        arr = []
+        while True:
+            user_in = input()
 
-        if user_in == 'end':
-            break
-        arr.append(int(user_in))
-    
-    if len(arr) > 0:
-        if cmd =="min":
-            print(calculate_min(arr))
-        elif cmd == "lcd":
-            print(calculate_lcm(arr))
-        elif cmd == 'max':
-            print(calculate_max(arr))
-        elif cmd == "average":
-            print(average(arr))
-        elif cmd == "gcd":
-            print(calculate_gcd(arr))
-        elif cmd == 'sum':
-            print(calculate_sum(arr))
-else:
-  print("Invalid command")
+            if user_in == 'end':
+                break
+            arr.append(int(user_in))
+
+        if len(arr) > 0:
+            if cmd == "min":
+                print(calculate_min(arr))
+            elif cmd == "lcd":
+                print(calculate_lcm(arr))
+            elif cmd == 'max':
+                print(calculate_max(arr))
+            elif cmd == "average":
+                print(calculate_average(arr))
+            elif cmd == "gcd":
+                print(calculate_gcd(arr))
+            elif cmd == 'sum':
+                print(calculate_sum(arr))
+    else:
+        print("Invalid command")
+
+if __name__ == "__main__":
+    main()
