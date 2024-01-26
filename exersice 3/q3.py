8e3d39b9bbf4418aa5d9b3e3649fe076b7e76c00
khafeshoarad <154825878+khafeshoarad@users.noreply.github.com>
2024-01-26 14:35:32 +0330

    Update q3.py
    refactored version

--- exersice 3/q3.py
+++ exersice 3/q3.py
@@ -1,19 +1,22 @@
 import re
+
 def remove_hash_after_at(input_string):
     while '@' in input_string and '#' in input_string:
         at_index = input_string.index('@')
         hash_index = input_string.find('#', at_index)
 
         if hash_index > at_index:
-            input_string = input_string[:hash_index] + input_string[hash_index+1:]
+            input_string = input_string[:hash_index] + input_string[hash_index + 1:]
         else:
             break
 
     input_string = re.sub(' +', ' ', input_string).strip()
-    
-    
-    return input_string.replace(r'\n', "\n")    
+    return input_string.replace(r'\n', "\n")
 
-input_string = input()
+def main():
+    input_string = input("Enter the text: ")
+    formatted_text = remove_hash_after_at(input_string)
+    print("Formatted Text:", formatted_text)
 
-print("Formatted Text: " + remove_hash_after_at(input_string))
+if __name__ == "__main__":
+    main()
