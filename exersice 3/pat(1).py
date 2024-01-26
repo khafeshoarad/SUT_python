9e930650e90950fdbedd278478fa83c3a0453a5d
khafeshoarad <154825878+khafeshoarad@users.noreply.github.com>
2024-01-26 14:41:34 +0330

    Update pat(1).py
    refactored version

--- exersice 3/pat(1).py
+++ exersice 3/pat(1).py
@@ -1,6 +1,6 @@
-vorudi = str(input())
-code = vorudi.split(" ")
-code.sort(key = lambda x: int(x[1:]))
-for i in code:
-    print (i[0],end="")
+vorudi = input("Enter a space-separated string: ")
+codes = vorudi.split()
+codes.sort(key=lambda x: int(x[1:]))
 
+result = "".join(item[0] for item in codes)
+print(result)
